f1f2029d9b6b66c75ca3ad11a7a23e159ddf84dc
Abeersharif111 <ab.a.r.a.sharif1@gmail.com>
2025-12-28 15:26:16 +0300

    ttt

---
+++ server.js
@@ -0,0 +1,30 @@
+//.dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
+//initialize the express
+const express = require('express');
+const app = express();
+
+//.mongoose
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const logger = require('morgan');    //middleware
+//connect mongoose
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+
+//middleware
+
+app.use(express.json());
+app.use(logger('dev')); // same as app.use(morgan("dev"))
+
+// Routes go here
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
